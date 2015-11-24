3850042e88d9c643ba6a51c832c90f697ed4942a
Суходько Александр <dalamar81@gmail.com>
2015-11-24 22:34:10 +0300

    Добавлены NuGet-пакеты

--- Beertown.Web/Beertown.Web/App_Start/BundleConfig.cs
+++ Beertown.Web/Beertown.Web/App_Start/BundleConfig.cs
@@ -21,11 +21,15 @@ namespace Beertown.Web
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap-select.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-select.css",
                       "~/Content/site.css"));
+
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
